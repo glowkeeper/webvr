c8c9adcf2662989c4b4b8d4990bbbf613f0ef565
glowkeeper <steve.huckle@gmail.com>
2023-02-07 12:39:47 +0000

    init project

---
+++ vr/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3443,
+    https: false,
+    // Uncomment to allow access from network
+    // (or use `npm run dev -- -- host=0.0.0.0`)
+    //host: '0.0.0.0',
+  }
+})
