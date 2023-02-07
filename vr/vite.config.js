2cb90661da54a9de136ab378228617620c3a6475
steve <steve.huckle@gmail.com>
2023-02-07 18:56:26 +0000

    install ssl

--- vr/vite.config.js
+++ vr/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
   server: {
     port: 3443,
-    https: false,
+    https: true,
     // Uncomment to allow access from network
     // (or use `npm run dev -- -- host=0.0.0.0`)
     //host: '0.0.0.0',
