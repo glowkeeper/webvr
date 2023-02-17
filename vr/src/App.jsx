60b685054044b2d67d3f5bf2f53f8d8c2c9a3661
steve <steve.huckle@gmail.com>
2023-02-17 11:28:36 +0000

    loaded mesh

--- vr/src/App.jsx
+++ vr/src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 
-import { ArcRotateCamera } from '@babylonjs/core/Cameras/arcRotateCamera.js'
+//import { ArcRotateCamera } from '@babylonjs/core/Cameras/arcRotateCamera.js'
+import { FreeCamera } from '@babylonjs/core/Cameras/freeCamera.js'
 import { Color3 } from '@babylonjs/core/Maths/math.color.js'
 import { Engine } from '@babylonjs/core/Engines/engine.js'
 import { EnvironmentHelper } from '@babylonjs/core/Helpers/environmentHelper.js'
@@ -11,6 +12,12 @@ import { Scene } from '@babylonjs/core/scene.js'
 import { StandardMaterial } from '@babylonjs/core/Materials/standardMaterial.js'
 import { Vector3 } from '@babylonjs/core/Maths/math.vector.js'
 import { WebXRDefaultExperience } from '@babylonjs/core/XR/webXRDefaultExperience.js'
+import { WebXRExperienceHelper } from '@babylonjs/core/XR/webXRExperienceHelper.js'
+
+import "@babylonjs/core/Helpers/sceneHelpers";
+import { SceneLoader } from '@babylonjs/core/Loading/sceneLoader'
+// import { DefaultLoadingScreen } from '@babylonjs/core/Loading/loadingScreen';
+import "@babylonjs/loaders/glTF";
 
 // Makes scene.beginAnimation is not a function error go away
 import "@babylonjs/core/Animations/animatable"
@@ -30,60 +37,110 @@ import '@babylonjs/core/Materials/Node/Blocks'
 import './App.css'
 
 function App() {
-  
-  useEffect(() => {
-    const canvas = document.getElementById("myCanvas");
 
-    // Create engine and a scene
-    const babylonEngine = new Engine(canvas, true)
-    const scene = new Scene(babylonEngine)
+  const createScene = (engine) => {
 
-    // Add a basic light
-    new HemisphericLight('light1', new Vector3(0, 2, 0), scene)
+    const scene = new Scene(engine)
+    scene.createDefaultEnvironment()
 
-    // Create a default environment (skybox, ground mesh, etc)
-    const envHelper = new EnvironmentHelper({
-      skyboxSize: 30,
-      groundColor: new Color3(0.5, 0.5, 0.5),
-    }, scene)
+    return scene
 
-    // Add a camera for the non-VR view in browser
-    const camera = new ArcRotateCamera("Camera", -(Math.PI / 4) * 3, Math.PI / 4, 10, new Vector3(0, 0, 0), scene);
-    camera.attachControl(true)
+  }
 
-    // Add a sphere to have something to look at
-    const sphereD = 1.0
-    const sphere = MeshBuilder.CreateSphere('xSphere', { segments: 16, diameter: sphereD }, scene)
-    sphere.position.x = 0
-    sphere.position.y = sphereD * 2
-    sphere.position.z = 0
-    const rMat = new StandardMaterial("matR", scene)
-    rMat.diffuseColor = new Color3(1.0, 0, 0)
-    sphere.material = rMat
+  const createCamera = (canvas, scene) => {
 
-    // Setup default WebXR experience
-    // Use the enviroment floor to enable teleportation
-    WebXRDefaultExperience.CreateAsync(scene, {
-      floorMeshes: [envHelper?.ground],
+    const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+    camera.setTarget(Vector3.Zero());
+    camera.attachControl(canvas, true);
+
+    return camera
+
+  }
+
+  const createLight = (scene) => {
+
+    //console.log('in light!')
+    
+    const light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene)
+    light.intensity = 0.7
+
+    return light
+  }
+
+  const loadAssets = async (scene) => {
+
+    try {
+
+      const result = await SceneLoader.ImportMeshAsync("", "/assets/", "designStudioWithCeiling.glb", scene) 
+      return result
+      
+    } catch (error) {
+
+      console.error('Error:', error)
+      return null
+      
+    } 
+  }
+
+  const setXR = async (scene) => {   
+
+    const env = scene.createDefaultEnvironment();
+    const xrHelper = await WebXRDefaultExperience.CreateAsync(scene, {
+      floorMeshes: [env.ground],
       optionalFeatures: true,
     })
 
-    // Run render loop
-    babylonEngine.runRenderLoop(() => {
+    if (!xrHelper.baseExperience) {
+      
+      console.log("no xr support")
+    
+    } else {
+
+      console.log("xr support! Good to go!")
+    }
+  }
+
+  const render = (engine, scene) => {
+    engine.runRenderLoop(() => {
       scene.render()
     })
+  }
+
+  const init = async () => {
+
+    const canvas = document.getElementById("myCanvas");
+    if ( canvas ) {
+
+      // Create engine and a scene
+      const babylonEngine = new Engine(canvas, true)
+
+      const scene = createScene(babylonEngine)
+      const camera = createCamera(canvas, scene)
+      const light = createLight(scene)
+
+      const mesh = await loadAssets(scene)
+      if ( mesh ) {
+
+        setXR(scene)
+        render(babylonEngine, scene)
+      }
+    }
+  }
+  
+  useEffect(() => { 
+
+    init()    
 
   }, []);
 
   return (
     <div className="App">
       <div>
-        <h1>HTML5 Canvas + React.js</h1>
+        <h1>Design Studio</h1>
         <canvas
           id="myCanvas"
-          width="200"
-          height="100"
-          style={{ border: "10px solid #d3d3d3" }}
+          width="1024"
+          height="768"
         >
           Your browser does not support the HTML canvas tag.
         </canvas>
